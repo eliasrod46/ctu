f8e496136704d20deaffedbcb6f51d8df36cbd2a
Elias Rodriguez <eliasrod46@gmail.com>
2024-02-05 14:31:16 -0300

    armando migracion "products"

--- database/migrations/2024_02_05_152153_create_products_table.php
+++ database/migrations/2024_02_05_152153_create_products_table.php
@@ -14,8 +14,31 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
+
             $table->id();
+            $table->string('code');
+            $table->string('description')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('size')->nullable();
+            $table->string('typeClothes')->nullable();
+            $table->string('color')->nullable();
+            $table->decimal('costPrice', 10, 2)->nullable();
+            $table->decimal('finalPrice', 10, 2)->nullable();
+            $table->string('image')->nullable();
+            $table->bigInteger('quantity')->nullable();
             $table->timestamps();
+
+            // $table->unsignedBigInteger('proveedor')->nullable();
+            // $table->unsignedBigInteger('producto_id')->nullable();
+            // $table->foreign('proveedor')->references('id')->on('proveedors');
+            // $table->foreign('producto_id')->references('id')->on('ventas')->onUpdate('cascade')->onDelete('cascade');
+
+            
+
+
+
+
+
         });
     }
 
